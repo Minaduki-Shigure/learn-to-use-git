b5e07753138b36062b3c80fda5b1f5167d5137e8
Minaduki Shigure <171180571@smail.nju.edu.cn>
2019-04-12 11:12:27 +0800

    Indent tested.

--- shift.c
+++ shift.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    int a=0xcffffff3;
-    printf("%x\n",a<<2);
-    printf("%x\n",0xcffffff3<<2);
-    return 0;
+	int a = 0xcffffff3;
+	printf("%x\n", a << 2);
+	printf("%x\n", 0xcffffff3 << 2);
+	return 0;
 }
