792322f67ea5e810fe935a4ca436a61a8348cff7
Minaduki Shigure <171180571@smail.nju.edu.cn>
2019-04-12 10:05:42 +0800

    Trying gpg keys

--- main.c
+++ main.c
@@ -7,4 +7,5 @@ int main(void)
     printf("Try to access GitLab with SSH key instead of HTTPS\n");
     printf("And now try to access via WWW\n");
     printf("Failed.\n");
+    printf("Trying to commit with a gpg key.\n");
 }
